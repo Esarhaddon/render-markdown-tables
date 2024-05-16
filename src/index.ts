522d73f20986161b51ff06462a8afe5fc9833453
Jackson <jaksondikison@gmail.com>
2024-05-15 18:01:43 -0700

    Add functions to global scope

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
+import { isMarkdownTable, renderMarkdownTable } from "./lib";
+
 export { isMarkdownTable, renderMarkdownTable } from "./lib";
+
+if (typeof window !== "undefined") {
+  const globalThis = window as unknown as Record<string, unknown>;
+
+  globalThis.isMarkdownTable = isMarkdownTable;
+  globalThis.renderMarkdownTable = renderMarkdownTable;
+}
